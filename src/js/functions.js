1f8aef9fd2602e600323ba17f99834cd06ef13b1
adrian.hernandez <adrian.hernandez@finvero.com>
2022-06-27 00:03:27 -0500

    fiRST Commit

--- src/js/functions.js
+++ src/js/functions.js
@@ -7,9 +7,4 @@ export const greet = (name) => {
 
   h1.innerText = `Hello, ${name}!`;
   document.body.append(h1);
-
-  //Img
- /*  const img = document.createElement("img");
-  img.src = logo;
-  document.body.append(img); */
 };
